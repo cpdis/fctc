6da73f0330b3c08a5fc666fa1d5a29d9ede927a2
Colin Dismuke <colin.dismuke@gmail.com>
2021-02-10 22:55:56 +0800

    :lipstick: Added dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,30 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        light: { raw: "(prefers-color-scheme: light)" },
+        dark: { raw: "(prefers-color-scheme: dark)" },
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, config }) {
+      addBase({
+        body: {
+          color: config("theme.colors.black"),
+          backgroundColor: config("theme.colors.white"),
+        },
+        "@screen dark": {
+          body: {
+            color: config("theme.colors.white"),
+            backgroundColor: config("theme.colors.black"),
+          },
+        },
+      });
+    },
+  ],
 };
